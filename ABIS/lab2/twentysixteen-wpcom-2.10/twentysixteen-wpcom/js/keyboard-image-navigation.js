10fd587bbb83a48e96fe556b101259e20a89cd9a
KuchN <mrkola45@gmail.com>
2026-02-25 12:26:00 +0300

    added lab2 for ABIS

---
+++ ABIS/lab2/twentysixteen-wpcom-2.10/twentysixteen-wpcom/js/keyboard-image-navigation.js
@@ -0,0 +1,30 @@
+/**
+ * Twenty Sixteen keyboard support for image navigation.
+ */
+
+( function() {
+	'use strict';
+
+	document.addEventListener( 'keydown', function ( e ) {
+		var url = null;
+		var focusedTag;
+		var el;
+
+		if ( e.code === 'ArrowLeft' || e.keyCode === 37 ) {
+			el = document.querySelector( '.nav-previous a' );
+			url = el && el.getAttribute( 'href' );
+		} else if ( e.code === 'ArrowRight' || e.keyCode === 39 ) {
+			el = document.querySelector( '.nav-next a' );
+			url = el && el.getAttribute( 'href' );
+		} else {
+			return;
+		}
+
+		focusedTag =
+			document.activeElement && document.activeElement.tagName.toLowerCase();
+
+		if ( url && focusedTag !== 'textarea' && focusedTag !== 'input' ) {
+			window.location = url;
+		}
+	} );
+} )();
