96ec2e6d28837dc5115132c850d27e12d7d4850c
KuchN <mrkola45@gmail.com>
2026-04-27 12:37:00 +0300

    added lab2 for TRWP

---
+++ TRWP/lab2/WebAPI/TDWA_01-01_2/wwwroot/TDWP_02-01.js
@@ -0,0 +1,54 @@
+const baseUrl = "http://localhost:20000/api/Save-JSON";
+
+function getInputData() {
+    return {
+        op: document.getElementById("op").value,
+        x: parseFloat(document.getElementById("x").value),
+        y: parseFloat(document.getElementById("y").value)
+    };
+}
+
+function showResult(data) {
+    document.getElementById("result").textContent =
+        JSON.stringify(data, null, 2);
+}
+
+async function sendGet() {
+    const res = await fetch(baseUrl, {
+        method: "GET"
+    });
+
+    const text = await res.text();
+    showResult(text);
+}
+
+async function sendPost() {
+    const res = await fetch(baseUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(getInputData())
+    });
+
+    const data = await res.json();
+    showResult(data);
+}
+
+async function sendPut() {
+    const res = await fetch(baseUrl, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(getInputData())
+    });
+
+    const data = await res.json();
+    showResult(data);
+}
+
+async function sendDelete() {
+    const res = await fetch(baseUrl, {
+        method: "DELETE"
+    });
+
+    const text = await res.text();
+    showResult(text);
+}
